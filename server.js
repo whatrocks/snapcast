ab0db60923d4092dc959f453a80852e97dd14661
way0750 <bruce0750@gmail.com>
2015-11-30 17:40:24 -0800

    change callback url to a relative url

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ var consumerSecret = "KgGnTXOHTCd9vDV4yV7pPsabqgGW92gt5lw7ZGWZvofVEjwKPQ";
 passport.use(new TwitterStrategy({
     consumerKey: consumerKey,
     consumerSecret: consumerSecret,
-    callbackURL: "https://www.snapcast.xyz/twitter/callback"
+    callbackURL: "twitter/callback"
   },
 
   function(token, tokenSecret, profile, done) {
